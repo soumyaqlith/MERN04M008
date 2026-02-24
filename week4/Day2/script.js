9f20ce609309973e5a3d15e48e420be67095f183
Developer-soumya <soumyamaharana89@gmail.com>
2026-02-24 11:51:49 +0530

    js 2nd class done

---
+++ week4/Day2/script.js
@@ -0,0 +1,96 @@
+console.log("welcome")
+
+a = 20;
+console.log(a)
+
+A = 40;
+console.log(A)
+
+a3 = 50
+console.log(a3)
+
+_ = 60;
+console.log(_);
+
+$ = 70;
+console.log($)
+
+// new = 80;
+// console.log(new)
+
+na3me = "Qlith";
+
+
+// var keyword
+
+// declaration at global scope
+
+var num1 = 40;
+
+console.log(num1)
+
+{
+    console.log("block", num1)
+}
+
+
+// declaration at block level
+{
+    var num2 = 60;
+    console.log(num2);
+}
+
+console.log("bahara",num2)
+
+//declaration at function level
+function dec(){
+    var val=70;
+    console.log(val)
+}
+
+// console.log("function bahar",val)
+
+dec()
+
+
+
+// let keyword
+
+// declare at global scope
+
+let variable="hello";
+
+console.log(variable)
+
+// declare at block 
+
+{
+    let v=50;
+    console.log(v)
+}
+
+// console.log(v)
+
+// declare at function
+
+
+
+// const keyword
+
+// declare at global scope
+const z=33;
+console.log(z)
+
+{
+    console.log(z)
+}
+
+// declare at block
+{
+    const age=90;
+    console.log(age);
+}
+
+console.log(age)
+
+
