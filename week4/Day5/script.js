fe096ed50d41959bf5afd5b8f30fc390e0f7f1c3
Developer-soumya <soumyamaharana89@gmail.com>
2026-02-27 12:47:29 +0530

    conditional statement done

---
+++ week4/Day5/script.js
@@ -0,0 +1,86 @@
+console.log("Hello");
+// condinitional statement
+
+let a = 2
+// if statement  // if-else
+if (a++ > 1 || ++a > 5) {
+    console.log("this is if block")
+} else {
+    console.log("this is else block")
+}
+
+console.log(a)
+
+// else- if ladder
+if (false) {
+
+} else if (false) {
+
+} else if (true) {
+
+} else {
+
+}
+
+// ternary operator
+// cond ? true : false
+
+console.log(true ? false ? "nexted true" : "nexted false" : "this is false")
+
+
+// nested cond
+let num = 8;
+if (num % 2 === 0) {
+    if (num % 3 == 0) {
+        console.log(num, "is divisible by 2 and 3")
+    } else {
+        console.log(num, "is not divisible by 3")
+    }
+} else {
+
+}
+
+// switch statement
+let choice = "B"
+switch (choice) {
+    case "A": console.log("this is A");
+        break;
+    case "B": console.log("this is B");
+        break;
+    case "C": console.log("this is C");
+        break;
+    default: console.log("this is default");
+        break;
+}
+if (false)
+    console.log("this is true ....");
+else
+    console.log("this is the else")
+
+
+console.log("this this is is s");
+
+
+// loop
+
+// console.log("hii");
+// console.log("hii");
+// console.log("hii");
+
+// for(let i =1;i<=100;i++){
+//     console.log("hii")
+// }
+
+// for loop
+// while loop
+// do-while loop
+
+// for(declaration with initialization ; cond ;updation){
+//    // code 
+// }
+
+for (let i = 1; i <= 5; i++) {
+    console.log("hii")
+}
+
+
