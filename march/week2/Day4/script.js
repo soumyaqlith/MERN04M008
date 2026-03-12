71f7b9c1611926f4b104614d26df3c29dd921841
Developer-soumya <soumyamaharana89@gmail.com>
2026-03-12 11:26:02 +0530

    dom object start

---
+++ march/week2/Day4/script.js
@@ -0,0 +1,13 @@
+let h1=document.getElementById("heading");
+console.log(h1.innerText);
+
+h1.innerText="this is is heading tag";
+console.log(h1.innerText)
+
+h1.innerText="this is is heading tag....";
+console.log(h1.innerText)
+
+let eles=document.getElementsByClassName("text");
+console.log(eles)
+
+
