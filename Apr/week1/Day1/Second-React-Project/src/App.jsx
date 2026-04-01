28c6f88af7c681b84067baeaa90bdd30c8e7934d
Developer-soumya <soumyamaharana89@gmail.com>
2026-04-01 12:23:23 +0530

    how to create the react project

---
+++ Apr/week1/Day1/Second-React-Project/src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Hello</h1>
+      <div className='container'>
+        <div className='items item1'>item 1</div>
+        <div className='items item2'>item 2</div>
+        <div className='items item3'>item 3</div>
+        <div className='items item4'>item 4</div>
+        <div className='items item5'>item 5</div>
+        <div className='items item6'>item 6</div>
+      </div>
+    </>
+  )
+}
+
+export default App
